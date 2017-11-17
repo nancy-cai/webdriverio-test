d8492c6ca4f9d286ec08974e507884b7cfd7966e
Nancy <nancy@openagent.com.au>
2017-11-17 16:29:34 +1100

    add beforeEach, add review test

--- test/example-app.js
+++ test/example-app.js
@@ -1,19 +1,33 @@
 var expect=require('chai').expect;
 
 describe('Robot page', function(){
-	it('should focus on the first invalid input field on error', function(){
-		browser
-		.url('/webdriverio-course-content')
-		.click('=Product Page');
+	
+	describe('Product page',function(){
+		beforeEach(function(){
+			browser.url('/webdriverio-course-content/product-page.html');
+		})
 		
-		var title=browser.getTitle();
-		expect(title).to.equal('Totally Not Evil Sentient Robot - Robot Parts Emporium');
+		it('should add a review when submitted properly',function(){
+			browser.setValue('#review-email','test@test.com');
+			browser.setValue('#review-content','Review test');
+			browser.submitForm('#review-content');
+			
+			var hasReview=browser.isExisting('.comment=Review test');
+			expect(hasReview,'comment text exists').to.be.true;
+		});
 		
-		var emailHasFocus=browser.hasFocus('#review-email');
-		expect(emailHasFocus,'email should not have focus').to.be.false;
-		
-		browser.submitForm('form');
-		emailHasFocus=browser.hasFocus('#review-email');
-		expect(emailHasFocus,'email should not have focus').to.be.true;
-	});
+		it('should focus on the first invalid input field on error', function(){
+			
+			var title=browser.getTitle();
+			expect(title).to.equal('Totally Not Evil Sentient Robot - Robot Parts Emporium');
+			
+			var emailHasFocus=browser.hasFocus('#review-email');
+			expect(emailHasFocus,'email should not have focus').to.be.false;
+			
+			browser.submitForm('form');
+			emailHasFocus=browser.hasFocus('#review-email');
+			expect(emailHasFocus,'email should not have focus').to.be.true;
+		});
+	})
+	
 });
